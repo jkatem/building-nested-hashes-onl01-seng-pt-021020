b5e95fa025de6c8d0c8bf113367bd2dc710712ab
Jen Miao <jenmiao@gmail.com>
2020-02-20 07:59:30 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -27,7 +27,7 @@ def third_challenge
           attitude: "hot-headed"
         }
       ]
-   }, 
+   },
    :capulet => {
       :patriarch => {
         name: "Lord Capulet",
@@ -59,5 +59,5 @@ def third_challenge
    }
   }
 
-  
+
 end
